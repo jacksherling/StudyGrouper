b48485afe802a2025920fc5d909dcdfe133640a7
jacksherl <65086686+jacksherl@users.noreply.github.com>
2020-10-09 16:49:14 -0400

    add library.js

---
+++ public/library.js
@@ -0,0 +1,13 @@
+// selector
+const _ = (str, all = false) => all ? 
+    document.querySelectorAll(str) :
+    $(str);
+
+// get/set attributes
+Element.prototype.attr = function (key, val = false) {
+    if (val) {
+        this.setAttribute(key, val);
+        return this;
+    }
+    return this.getAttribute(key);
+}
