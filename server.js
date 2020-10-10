349a70caf933cd4491c1e88dfa77ef854caf1b06
Yixuan Li <lxd520ANNA@gmail.com>
2020-10-10 11:54:50 -0400

    backup before lunch break

--- server.js
+++ server.js
@@ -1,44 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-
-passport.use(
-	new LocalStrategy(function (email, password, done) {
-		User.findOne({ email: email }, function (err, user) {
-			if (err) {
-				return done(err);
-			}
-			if (!user) {
-				return done(null, false, {
-					message: "User with this email doesn't exist.",
-				});
-			}
-			if (!user.validPassword(password)) {
-				return done(null, false, { message: "Incorrect password." });
-			}
-			return done(null, user);
-		});
-	})
-);
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(
+	session({
+		secret: "Thisismylittlesecret.",
+		resave: false,
+		saveUninitialized: false,
+	})
+);
 
 mongoose.connect(
 	"mongodb+srv://Yixuan:abAB12!@@cluster0.boczf.mongodb.net/test?retryWrites=true&w=majority",
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
+		useCreateIndex: true,
 	}
 );
 
-const PORT = process.env.port || 3000;
-
 const User = require("./models/User");
+
+const PORT = process.env.port || 3000;
 
 app.get("/", (req, res) => {
 	res.render("index");
@@ -53,7 +43,11 @@ app.post("/register", (req, res) => {
 	const newUser = new User({
 		email: data.email,
 		name: data.name,
-		password: data.password,
+		password: bcrypt.hashSync(
+			data.password,
+			bcrypt.genSaltSync(saltRounds),
+			null
+		),
 		school: data.school,
 		phone: data.pn,
 		classes: {
@@ -63,14 +57,28 @@ app.post("/register", (req, res) => {
 			socialStudies: data.socialStudies,
 		},
 	});
+
+	newUser.save();
+	res.redirect("/login");
 });
 
 app.get("/login", (req, res) => {
 	res.render("login");
 });
 
-app.post("/login", passport.authenticate("local"), function (req, res) {
-	res.redirect("/");
+app.post("/login", function (req, res) {
+	User.findOne({ email: req.body.email }, (err, foundUser) => {
+		if (err) {
+			console.log(err);
+		} else {
+			if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+				req.user = foundUser;
+				res.redirect("/");
+			} else {
+				res.redirect("/login");
+			}
+		}
+	});
 });
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
